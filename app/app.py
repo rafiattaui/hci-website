403c5d6778e3d7101e2ec4963a4f5a6ccae2776d
at@atta <athallah.rafi13@gmail.com>
2024-11-22 00:39:32 +0700

    testing

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h4>Hello, this is the main page!</h4>"
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
