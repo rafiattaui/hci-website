370e707617c1aad646cc1e1ed47084052b5aedf1
at@atta <athallah.rafi13@gmail.com>
2024-11-25 17:19:57 +0700

    second commit

--- app/app.py
+++ app/app.py
@@ -1,10 +1,9 @@
-from flask import Flask, redirect, url_for, render_template
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("recipes.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
