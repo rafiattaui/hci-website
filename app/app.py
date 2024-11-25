86a459c22bf5e8636b116f19eb958982c64e45e9
at@atta <athallah.rafi13@gmail.com>
2024-11-25 18:22:49 +0700

    minor changes

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from firebase_admin import credentials, firestore
 app = Flask(__name__)
 
 # Initialize Firebase
-cred = credentials.Certificate('recipe-page-project-firebase-adminsdk-v39uz-c296d89862.json')
+cred = credentials.Certificate('recipe-page-project-firebase-adminsdk.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
